a4fec5ad0367cad6e0d8ab4aef76594fba34135b
Evgenii Krasikov <krasikov.ev2@gmail.com>
2024-03-01 19:35:51 +0500

    fix connection pool

--- src/main/java/by/asckarugin/Utils/ConnectionManager.java
+++ src/main/java/by/asckarugin/Utils/ConnectionManager.java
@@ -1,13 +1,9 @@
 package by.asckarugin.Utils;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
@@ -21,10 +17,9 @@ public class ConnectionManager {
 
     private static final String POOL_SIZE_KEY = "db.pool.size";
 
-    private static final Integer DEFAULT_POOL_SIZE = 25;
+    private static final Integer DEFAULT_POOL_SIZE = 20;
 
     private static BlockingQueue<Connection> pool;
-    private static List<Connection> sourceConnections;
 
     static{
         loadDriver();
@@ -45,14 +40,12 @@ public class ConnectionManager {
         String poolSize = PropertiesUtil.get(POOL_SIZE_KEY);
         int size = poolSize == null ? DEFAULT_POOL_SIZE : Integer.parseInt(poolSize);
         pool = new ArrayBlockingQueue<>(size);
-        sourceConnections = new ArrayList<>(size);
         for (int i = 0; i < size; i++) {
             Connection connection = open();
             Connection proxyConnection = (Connection) Proxy.newProxyInstance(ConnectionManager.class.getClassLoader(), new Class[]{Connection.class},
                     (proxy, method, args) -> method.getName().equals("close")
                         ? pool.add((Connection) proxy) : method.invoke(connection, args));
             pool.add(proxyConnection);
-            sourceConnections.add(connection);
         }
     }
 
@@ -73,16 +66,6 @@ public class ConnectionManager {
             );
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        }
-    }
-
-    public static void closePool(){
-        for (Connection sourceConnection : sourceConnections) {
-            try {
-                sourceConnection.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
         }
     }
 }
